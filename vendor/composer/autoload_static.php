8ade35991c5d15a9d5bbbd06f51ae22977730f26
ehabtalaat27 <ehab.talaat@rqoay.com>
2025-05-13 17:26:47 +0300

    change guzzle version

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,6 +9,7 @@ class ComposerStaticInitfee40d8fdbb31ee302d3faeabc5073a1
     public static $files = array (
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
